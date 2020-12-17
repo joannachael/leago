1c2d0a1d6a18e48e7c76007e066b2e71bffb157f
mikki <mikki@gmail.com>
2020-12-17 15:57:16 +0200

    queues, tiers and divisions

---
+++ leagues.go
@@ -0,0 +1,20 @@
+package leago
+
+const (
+	RANKED_SOLO_5x5 = "RANKED_SOLO_5x5"
+	RANKED_FLEX_SR  = "RANKED_FLEX_SR"
+	RANKED_FLEX_TT  = "RANKED_FLEX_TT"
+	CHALLENGER      = "CHALLENGER"
+	GRANDMASTER     = "GRANDMASTER"
+	MASTER          = "MASTER"
+	DIAMOND         = "DIAMOND"
+	PLATINUM        = "PLATINUM"
+	GOLD            = "GOLD"
+	SILVER          = "SILVER"
+	BRONZE          = "BRONZE"
+	IRON            = "IRON"
+	I               = "I"
+	II              = "II"
+	III             = "III"
+	IV              = "IV"
+)
