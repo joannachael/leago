af55f85cb0bbd6b1dae5bcb8ea678e658c5d34a5
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-18 09:30:42 +0200

    resized method

--- league-exp-v4.go
+++ league-exp-v4.go
@@ -5,9 +5,7 @@ import (
 	"strconv"
 )
 
-func (c *client) GetEntriesByQueueTierAndDivisionEXP(
-	ctx context.Context, region, queue, tier, division string, page int,
-) ([]entry, error) {
+func (c *client) GetEntriesByQueueTierAndDivisionEXP(ctx context.Context, region, queue, tier, division string, page int) ([]entry, error) {
 	var entries []entry
 	address := "/lol/league-exp/v4/entries/" + queue + "/" + tier + "/" + division + "?page=" + strconv.Itoa(page)
 	if err := c.doRequest(ctx, region, address, &entries); err != nil {
