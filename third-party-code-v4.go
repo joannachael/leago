79ca90e16ea3f289769f06dce7c8ad68e7b2481e
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:07:43 +0200

    removed mistake in expression

--- third-party-code-v4.go
+++ third-party-code-v4.go
@@ -2,9 +2,9 @@ package leago
 
 import "context"
 
-func (c *client) GetPartyCodeBySummonerID(ctx context.Context, region, ID string) (string, error) {
+func (c *client) GetPartyCodeBySummonerId(ctx context.Context, region, Id string) (string, error) {
 	var code string
-	if err := c.doRequest(ctx, region, "/lol/platform/v4/third-party-code/by-summoner/" + ID, &code); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/platform/v4/third-party-code/by-summoner/" + Id, &code); err != nil {
 		return "", err
 	}
 	return code, nil
