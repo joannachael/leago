ae13fd569d0d6c119e5ee2e8fae49f37e2c04fb3
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:24:52 +0200

    Update summoner-v4.go

--- summoner-v4.go
+++ summoner-v4.go
@@ -28,17 +28,17 @@ func (c *client) GetSummonerByAccountId(ctx context.Context, region, accountId s
 	return &summoner, nil
 }
 
-func (c *client) GetSummonerByName(ctx context.Context, region, name string) (*summoner, error) {
+func (c *client) GetSummonerByName(ctx context.Context, region, summonerName string) (*summoner, error) {
 	var summoner summoner
-	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-name/" + name, &summoner); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-name/" + summonerName, &summoner); err != nil {
 		return nil, err
 	}
 	return &summoner, nil
 }
 
-func (c *client) GetSummonerByPUUID(ctx context.Context, region, PUUID string) (*summoner, error) {
+func (c *client) GetSummonerByPUUID(ctx context.Context, region, summonerPUUID string) (*summoner, error) {
 	var summoner summoner
-	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-puuid/" + PUUID, &summoner); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-puuid/" + summonerPUUID, &summoner); err != nil {
 		return nil, err
 	}
 	return &summoner, nil
