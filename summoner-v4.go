9df5f36b60db5fb43545e9154bdfe5d785c5ac18
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:08:48 +0200

    removed mistake in expression

--- summoner-v4.go
+++ summoner-v4.go
@@ -3,26 +3,26 @@ package leago
 import "context"
 
 type summoner struct {
-	ID            string `json:"id"`
-	AccountID     string `json:"accountId"`
+	Id            string `json:"id"`
+	AccountId     string `json:"accountId"`
 	PUUID         string `json:"puuid"`
 	Name          string `json:"name"`
-	IconID        int    `json:"profileIconId"`
+	IconId        int    `json:"profileIconId"`
 	RevisionDate  int64  `json:"revisionDate"`
 	SummonerLevel int64  `json:"summonerLevel"`
 }
 
-func (c *client) GetSummonerByID(ctx context.Context, region, ID string) (*summoner, error) {
+func (c *client) GetSummonerById(ctx context.Context, region, summonerId string) (*summoner, error) {
 	var summoner summoner
-	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/" + ID, &summoner); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/" + summonerId, &summoner); err != nil {
 		return nil, err
 	}
 	return &summoner, nil
 }
 
-func (c *client) GetSummonerByAccountID(ctx context.Context, region, accountID string) (*summoner, error) {
+func (c *client) GetSummonerByAccountId(ctx context.Context, region, accountId string) (*summoner, error) {
 	var summoner summoner
-	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-account/" + accountID, &summoner); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/summoner/v4/summoners/by-account/" + accountId, &summoner); err != nil {
 		return nil, err
 	}
 	return &summoner, nil
