37a1faa9a842ca3fc37bf674c2495082e72ee1c4
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:07:18 +0200

    removed mistake in expression

--- clash-v1.go
+++ clash-v1.go
@@ -7,10 +7,10 @@ import (
 
 type (
 	team struct {
-		ID           string        `json:"id"`
-		TournamentID int           `json:"tournamentId"`
+		Id           string        `json:"id"`
+		TournamentId int           `json:"tournamentId"`
 		Name         string        `json:"name"`
-		IconID       int           `json:"iconId"`
+		IconId       int           `json:"iconId"`
 		Tier         int           `json:"tier"`
 		Captain      string        `json:"captain"`
 		Abbreviation string        `json:"abbreviation"`
@@ -18,39 +18,39 @@ type (
 	}
 
 	clashPlayer struct {
-		SummonerID string `json:"summonerId"`
-		TeamID     string `json:"teamId"`
+		SummonerId string `json:"summonerId"`
+		TeamId     string `json:"teamId"`
 		Position   string `json:"position"`
 		Role       string `json:"role"`
 	}
 
 	tournament struct {
-		ID               int     `json:"id"`
-		ThemeID          int     `json:"themeId"`
+		Id               int     `json:"id"`
+		ThemeId          int     `json:"themeId"`
 		NameKey          string  `json:"nameKey"`
 		NameKeySecondary string  `json:"nameKeySecondary"`
 		Schedule         []phase `json:"schedule"`
 	}
 
 	phase struct {
-		ID               int   `json:"id"`
+		Id               int   `json:"id"`
 		RegistrationTime int64 `json:"registrationTime"`
 		StartTime        int64 `json:"startTime"`
 		Cancelled        bool  `json:"cancelled"`
 	}
 )
 
-func (c *client) GetPlayersBySummonerID(ctx context.Context, region, ID string) ([]clashPlayer, error) {
+func (c *client) GetPlayersBySummonerId(ctx context.Context, region, summonerId string) ([]clashPlayer, error) {
 	var players []clashPlayer
-	if err := c.doRequest(ctx, region, "/lol/clash/v1/players/by-summoner/" + ID, &players); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/clash/v1/players/by-summoner/" + summonerID, &players); err != nil {
 		return nil, err
 	}
 	return players, nil
 }
 
-func (c *client) GetTeamByID(ctx context.Context, region, ID string) (*team, error) {
+func (c *client) GetTeamById(ctx context.Context, region, Id string) (*team, error) {
 	var team team
-	if err := c.doRequest(ctx, region, "/lol/clash/v1/teams/" + ID, &team); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/clash/v1/teams/" + Id, &team); err != nil {
 		return nil, err
 	}
 	return &team, nil
@@ -64,17 +64,17 @@ func (c *client) GetTournaments(ctx context.Context, region string) ([]tournamen
 	return tournaments, nil
 }
 
-func (c *client) GetTournamentByTeamID(ctx context.Context, region, ID string) (*tournament, error) {
+func (c *client) GetTournamentByTeamId(ctx context.Context, region, Id string) (*tournament, error) {
 	var tournament tournament
-	if err := c.doRequest(ctx, region, "/lol/clash/v1/tournaments/by-team/" + ID, &tournament); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/clash/v1/tournaments/by-team/" + Id, &tournament); err != nil {
 		return nil, err
 	}
 	return &tournament, nil
 }
 
-func (c *client) GetTournamentByID(ctx context.Context, region string, ID int) (*tournament, error) {
+func (c *client) GetTournamentById(ctx context.Context, region string, Id int) (*tournament, error) {
 	var tournament tournament
-	if err := c.doRequest(ctx, region, "/lol/clash/v1/tournaments/" + strconv.Itoa(ID), &tournament); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/clash/v1/tournaments/" + strconv.Itoa(Id), &tournament); err != nil {
 		return nil, err
 	}
 	return &tournament, nil
