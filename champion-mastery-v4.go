a9a5dd764753216eb3ee7a88f6b4ba5be57728d1
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:23:45 +0200

    removed some shortcomings

--- champion-mastery-v4.go
+++ champion-mastery-v4.go
@@ -6,8 +6,8 @@ import (
 )
 
 type championMastery struct {
-	SummonerID                   string `json:"summonerId"`
-	ChampionID                   int64  `json:"championId"`
+	SummonerId                   string `json:"summonerId"`
+	ChampionId                   int64  `json:"championId"`
 	ChampionLevel                int    `json:"championLevel"`
 	ChampionPoints               int    `json:"championPoints"`
 	LastPlayTime                 int64  `json:"lastPlayTime"`
@@ -17,28 +17,28 @@ type championMastery struct {
 	TokensEarned                 int    `json:"tokensEarned"`
 }
 
-func (c *client) GetMasteriesBySummonerID(ctx context.Context, region, ID string) ([]championMastery, error) {
+func (c *client) GetMasteriesBySummonerId(ctx context.Context, region, summonerId string) ([]championMastery, error) {
 	var masteries []championMastery
-	address := "/lol/champion-mastery/v4/champion-masteries/by-summoner/"
-	if err := c.doRequest(ctx, region, address + ID, &masteries); err != nil {
+	address := "/lol/champion-mastery/v4/champion-masteries/by-summoner/" + summonerId
+	if err := c.doRequest(ctx, region, address, &masteries); err != nil {
 		return nil, err
 	}
 	return masteries, nil
 }
 
-func (c *client) GetMasteryBySummonerAndChampionID(ctx context.Context, region, summonerID string, championID int) (*championMastery, error) {
+func (c *client) GetMasteryBySummonerAndChampionId(ctx context.Context, region, summonerId string, championId int) (*championMastery, error) {
 	var mastery championMastery
 	api := "/lol/champion-mastery/v4/champion-masteries/by-summoner/"
-	address := api + summonerID + "/by-champion/" + strconv.Itoa(championID)
+	address := api + summonerId + "/by-champion/" + strconv.Itoa(championId)
 	if err := c.doRequest(ctx, region, address, &mastery); err != nil {
 		return nil, err
 	}
 	return &mastery, nil
 }
 
-func (c *client) GetScoreBySummonerID(ctx context.Context, region, ID string) (int, error) {
+func (c *client) GetScoreBySummonerId(ctx context.Context, region, summonerId string) (int, error) {
 	var score int
-	if err := c.doRequest(ctx, region, "/lol/champion-mastery/v4/scores/by-summoner/" + ID, &score); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/champion-mastery/v4/scores/by-summoner/" + summonerId, &score); err != nil {
 		return 0, err
 	}
 	return score, nil
