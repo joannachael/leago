207a18a196c247c98ac033250c897b8196686da9
Michael Might <64410120+mikemight@users.noreply.github.com>
2020-12-20 13:24:14 +0200

    Update third-party-code-v4.go

--- third-party-code-v4.go
+++ third-party-code-v4.go
@@ -2,9 +2,9 @@ package leago
 
 import "context"
 
-func (c *client) GetPartyCodeBySummonerId(ctx context.Context, region, Id string) (string, error) {
+func (c *client) GetPartyCodeBySummonerId(ctx context.Context, region, summonerId string) (string, error) {
 	var code string
-	if err := c.doRequest(ctx, region, "/lol/platform/v4/third-party-code/by-summoner/" + Id, &code); err != nil {
+	if err := c.doRequest(ctx, region, "/lol/platform/v4/third-party-code/by-summoner/" + summonerId, &code); err != nil {
 		return "", err
 	}
 	return code, nil
